dcd62141d628a52ca8acec92040f193761ad73d9
sahn1978 <108362148+sahn1978@users.noreply.github.com>
2026-05-22 12:25:26 -0400

    Update password hashes in config.js

--- src/config.js
+++ src/config.js
@@ -17,5 +17,5 @@
 //   but does not stop a determined viewer with browser dev tools.
 //   Do not rely on this for protecting sensitive data.
 // =============================================================
-
-export const ADMIN_PASSWORD_HASH = ''
+export const SITE_PASSWORD_HASH = '3a2922d56f4e474a8b257c50992c957452c7cf643dd6d8f30d04a57f2c25a283'
+export const ADMIN_PASSWORD_HASH = '3a2922d56f4e474a8b257c50992c957452c7cf643dd6d8f30d04a57f2c25a283'
